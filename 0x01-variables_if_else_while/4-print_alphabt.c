536dea1b0214cb33a4bb42c1dcb05378282cc774
Merna <HanaaHussein414@gmail.com>
2023-09-08 13:48:38 -0700

    O

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	/*lower case*/
+	while (ch <= 'z')
+	{
+	if (ch == 'e' || ch == 'q')
+	{
+	ch++;
+	}
+	putchar(ch);
+	ch++;
+	}
