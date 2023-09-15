39ae8517df8efac0b357bf0d9e3d38ccfe89528c
Merna <HanaaHussein414@gmail.com>
2023-09-15 02:10:06 -0700

    P

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -24,10 +24,17 @@ double _sqrt(double x)
 }
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *largest_prime_factor - finds and prints the largest
+ *	prime factor of number (num)
+ *@num: number to find
  */
+void largest_prime_factor(long int num)
+{
+	int prmNu, largest;
+
+	/* frist divide with the smallest prime number (two) */
+	while (num % 2 = 0)
+		num = num / 2;
 int main(void)
 {
 	largest_prime_factor(612852475143)
