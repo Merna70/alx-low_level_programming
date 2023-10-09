8ec0041b28ff410ca2fc31c0617e251329bbf774
Merna <hanaahussein414@gmail.com>
2023-10-09 16:15:56 -0700

    O

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * *_memset - function allocates memory for an array
+ *@s: pointer
+ *@b: constant
+ *@n: max byte to use
+ *Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+
+/**
+ * *_calloc - function
+ * @nmemb: aaray len
+ * @size: size of e element
+ * Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	m = malloc(sizeof(int) * nmemb);
+
+	if (m == 0)
+		return (NULL);
+
+	_memset(m, 0, sizeof(int) * nmemb);
+
+	return (m);
+}
