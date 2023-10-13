9ae8b1d9822a8ce51cad9a244956863b56ff37cb
Merna <hanaahussein414@gmail.com>
2023-10-12 17:38:55 -0700

    D

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,56 @@
 #include "variadic_functions.h"
 
 /**
+ *format_char - function char
+ *@separator: the string separator
+ *@ap: argument
+*/
+
+void format_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+ *format_int - function integer
+ *@separator: the string operator
+ *@ap: argument pointer
+*/
+
+void format_int(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, int));
+}
+
+/**
+ *format_float - function  float
+ *@separator: the string separator
+ *@ap: argument pointer
+ *
+*/
+
+void format_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, double));
+}
+
+/**
+ *format_string - function strings
+ *@separator: the string separator
+ *@ap: argument pointer
+*/
+
+void format_string(char *separator, va_list ap)
+{
+	char *str = va_arg(ap, char *);
+
+	switch ((int)(!str))
+	case 1:
+		str = "(nil)";
+	printf("%s%s", separator, str);
+}
+
+/**
  *print_all - function that prints anything
  *@format: the format string
  *
