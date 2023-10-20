bb2f8a48482ca01868bb2375f4d461efa2c1de4b
Merna70 <hanaahussein414@gmail.com>
2023-10-19 17:57:18 -0700

    S

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-void _constructor(void) _attribute_ ((constructor));
+void _constructor(void)_attribute_((constructor));
 
 /**
  * _constructor - function
@@ -11,6 +11,5 @@ void _constructor(void) _attribute_ ((constructor));
 void _constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n"
-		"(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.\n")
+		"I bore my house upon my back!\n");
 }
