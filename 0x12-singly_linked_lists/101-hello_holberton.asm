c4e199ce879c01a3560d5dbdb09911c565dd5af1
Merna70 <hanaahussein414@gmail.com>
2023-10-19 18:42:55 -0700

    S

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,9 +3,8 @@ section		.text
 	global  main
 
 main:
-	mov	edi, message
-	call  printf
-	mov	eax, 0
-
+	mov		edi, message
+	mov		eax, 0
+	call    printf
 section		 .data
 	message: db 'Hello, Holberton', 0xa, 0
