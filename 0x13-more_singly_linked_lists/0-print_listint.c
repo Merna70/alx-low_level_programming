24fda341ade44d23638af0873d865821a374a024
Merna70 <hanaahussein414@gmail.com>
2023-10-24 08:25:45 -0700

    E

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,38 @@
 #include "lists.h"
-
 /**
-*print_listint - function
-*@h: pointer the head node
-*
-*Return: int
+ *_strlen - function
+ *@s: pointer to the string to be checked
+ *
+ *Return: the length of the string
 */
 
-size_t print_listint(const listint_t *h)
+int _strlen(char *s)
 {
-	size_t ele_num = 0;
+	int len = 0;
 
-	while (h)
+	if (!s)
+		return (0);
+	while (*s++)
+		len++;
+	return (len);
+}
+
+/**
+ *print_list - print
+ *@h: pointer to header node
+ *
+ * Return: size of the list
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t z = 0;
+
+	if (h)
 	{
-		printf("%d\n", h->n);
-		ele_num++;
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
+		z++;
 	}
-	return (ele_num);
+	return (z);
 }
