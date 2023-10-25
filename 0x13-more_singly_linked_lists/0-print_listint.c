549664cdf123c5a67f1e945bf4c6199c53e4f979
Merna70 <HanaaHussein414@gmail.com>
2023-10-24 19:41:17 -0700

    S

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,23 @@
+#include "lists.h"
 
+/**
+*print_listint - print linked lists
+
+
+*@h: pointer the head node
+*
+*Return: int
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i_);
+}
