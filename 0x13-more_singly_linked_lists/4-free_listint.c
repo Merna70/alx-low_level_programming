f867cbf255110982d42dbceeed82fdea8113afc9
Merna70 <HanaaHussein414@gmail.com>
2023-10-24 17:29:52 -0700

    P

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+*free_listint - function
+*@head: the head list
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
