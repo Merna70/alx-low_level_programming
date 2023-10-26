0cc1e201a1742794ced5c354fb76c8e1741e7533
Merna70 <HanaaHussein414@gmail.com>
2023-10-25 19:59:43 -0700

    K

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,22 +7,18 @@
  * Return: address of head
  */
 
-listint_t *reverse_listint(listint_t **head);
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *node = NULL, *next = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-	if (!head || !*head)
-
-		return (NULL);
-
-	node = *head;
-	*head = Null;
-	while (node)
+	if (*head)
 	{
-		next = node->next;
-		node->next = *head;
-		*head = node;
-		node = next;
-	}
+	next = (*head)->next;
+	(*head)->next = prev;
+	prev = *head;
+	*head = next;
+	{
+	*head = prev;
 	return (*head);
 }
